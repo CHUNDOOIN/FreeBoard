2c3008bd50ed5a40d7cbc4bb199c77711dd0b30c
CHUNDOOIN <chun.doo.in@gmail.com>
2022-03-20 18:18:06 +0900

    22.03.20 작업중

--- styles/detail_board.js
+++ styles/detail_board.js
@@ -5,7 +5,7 @@ export const Wrap = styled.div`
   width: 1200px;
   height: 1847px;
   margin: 100px auto;
-  padding: 60px 100px 100px 100px;
+  padding: 60px 102px 100px 102px;
 
   background: #ffffff;
   box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
